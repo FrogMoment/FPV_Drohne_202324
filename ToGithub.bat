1d8e53e2ebb0568436fe95a11010f490620c4fd3
Maximilian Lendl <maximilian.lendl@gmail.com>
2023-10-18 13:48:59 +0200

    test bat

---
+++ ToGithub.bat
@@ -0,0 +1,18 @@
+:: Add all changes to the staging area
+git add --all
+
+:: remove folders
+git rm --cached -r "MDDS Testing"/MDK-ARM/"MDDS Testing"
+git rm --cached -r "Drohne Lendl"/MDK-ARM/"Drohne Lendl"
+
+:: Prompt for the commit message
+set /p commit_message="Enter your commit message: "
+
+:: Commit the changes with the provided commit message
+git commit -m "%commit_message%"
+
+:: Push to the master branch
+git push origin master
+
+:: Pause to view the output (you can remove this line if not needed)
+pause
